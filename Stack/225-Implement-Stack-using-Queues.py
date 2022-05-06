a85cca3a4d0dade8e2eed3db6342520ac26b534b
DharaneeswaranR <dharaneeshram@gmail.com>
2022-05-06 19:39:18 +0530

    added 225-Implement-Stack-using-Queues.py

---
+++ Stack/225-Implement-Stack-using-Queues.py
@@ -0,0 +1,26 @@
+class MyStack:
+    def __init__(self):
+        self.queue = deque()
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+        for i in range(len(self.queue) - 1):
+            self.queue.append(self.queue.popleft())
+
+    def pop(self) -> int:
+        return self.queue.popleft()
+
+    def top(self) -> int:
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        return len(self.queue) == 0
+
+
+"""
+Approach:
+
+Rotate the queue whenever new element is push such that the front element is
+the last pushed element.
+
+"""
