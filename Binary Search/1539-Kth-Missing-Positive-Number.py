0bea13d9223268e84c135db0f64d70ecd65dbc7e
DharaneeswaranR <dharaneeshram@gmail.com>
2023-03-06 19:45:01 +0530

    added 1539-Kth-Missing-Positive-Number.py

---
+++ Binary Search/1539-Kth-Missing-Positive-Number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        left, right = 0, len(arr) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+
+            if arr[mid] - (mid + 1) < k:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return left + k
+
+
+"""
+Find the number of missing numbers to the left using arr[mid] - (mid + 1) 
+
+"""
