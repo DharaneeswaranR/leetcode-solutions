ee7717bebb784b23d0a54b9631fb642860852c92
DharaneeswaranR <dharaneeshram@gmail.com>
2023-05-24 10:28:13 +0530

    added 701-Insert-into-a-Binary-Search-Tree.py (recursive soln)

--- Trees/701-Insert-into-a-Binary-Search-Tree.py
+++ Trees/701-Insert-into-a-Binary-Search-Tree.py
@@ -24,4 +24,17 @@ class Solution:
             prev.left = TreeNode(val, None, None)
             
         return root
+    
         
+# Recursive solution
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return TreeNode(val)
+
+        if root.val > val:
+            root.left = self.insertIntoBST(root.left, val)
+        else:
+            root.right = self.insertIntoBST(root.right, val)
+
+        return root
